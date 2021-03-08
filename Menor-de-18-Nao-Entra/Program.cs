4ee3f4139650461dd49d7402e8dcc6401d777b25
Guilherme Bini <77159678+Bini19@users.noreply.github.com>
2021-03-08 15:32:11 -0300

    Add files via upload

---
+++ Menor-de-18-Nao-Entra/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Menor_de_18_Nao_Entra
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+             Processar();
+
+             Console.ReadKey();
+         }
+        
+
+        private static void Processar()
+        {
+            int Idade;
+            Console.Write("Informe a sua idade: ");
+            int.TryParse(Console.ReadLine(), out Idade);
+
+            if (Idade >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Acesso permitdo");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Acesso Negado");
+            }
+        }
+    }   
+}
